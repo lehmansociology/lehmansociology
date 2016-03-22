3b170725c570bda93f739b15c966dcd7ab88635a
Elin Waring <elin.waring@gmail.com>
2016-03-22 10:41:39 -0400

    Moving things around and adding regression1 template.

---
+++ R/create_educ_poverty_data.R
@@ -0,0 +1,33 @@
+# Create the education and poverty data set for SOC 345
+
+#' Extract and merge data
+#'
+#' This function creates and assigns a custom class to handle numbers with commas.
+#' @keywords poverty education data
+#' @export
+#' @examples
+#' create_educ_poverty_data_function()
+
+
+create_educ_poverty_data <- function()
+{
+
+    library(googlesheets)
+    library(dplyr)
+    poverty13<-select (poverty.states, FIPStxt, Area_Name, PCTPOVALL_2013)
+    poverty13$FIPS.Code<-as.integer(poverty13$FIPStxt)
+
+    lessthanhighschool13 <- select(education.states, Area.name, FIPS.Code,                                Percent.of.adults.with.less.than.a.high.school.diploma..2009.2013)
+
+    education_and_poverty<-merge(poverty13, lessthanhighschool13,
+                                 by.x='FIPS.Code', by.y='FIPS.Code')
+
+    # First let's create the region data set
+    gs_region<-gs_url('https://docs.google.com/spreadsheets/d/1h_jY4A44WoSLkrqhwZZ9oJh51N2GybwVvGgEaY3n2gc/pubhtml')
+    region_data<-gs_read(gs_region)
+
+    education_and_poverty <- merge(education_and_poverty, region_data,
+                                   by.x='FIPS.Code', by.y='FIPS.Code')
+    education_and_poverty
+
+}
